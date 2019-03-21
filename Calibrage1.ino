d2bfe979c78b8bba0aa5fd5c13ac415cde688432
mehdiberriri <mehdi.berriri@etu.unice.fr>
2019-03-21 12:50:55 +0100

    #8 Esssayer de s'emparer d'un objet avec le Bras

--- Calibrage1.ino
+++ Calibrage1.ino
@@ -3,12 +3,14 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-// Die eingestellen Minimal- und Maximalwerte können, je nach Motor variieren
-#define SERVOMIN  0 // Minimale-Pulslänge
-#define SERVOMAX  100 // Maximale-Pulslänge
+//moteur 1 de 0 à 450
+//moteur 5 de 220 à 400
+
+#define SERVOMIN  210 
+#define SERVOMAX  400 
 
 // Servozähler
-uint8_t servonum = 0;
+uint8_t servonum = 5;
 
 void setup() {
   Serial.begin(9600);
@@ -16,26 +18,86 @@ void setup() {
 
   pwm.begin();
   
-  pwm.setPWMFreq(60);  // Analoge Servomotoren haben eine Aktualisierungsrate von 60Hz
+  pwm.setPWMFreq(60);
 
   yield();
 }
 
 void loop() {
-  // jeden Motor einzelnd ansteuern
+//baisser le bras
   Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
+  for (uint16_t pulselen = 0; pulselen < 300; pulselen++) {
+    pwm.setPWM(0, 0, pulselen); //servo 0
+    delay(10);
   }
 
-  delay(1000);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-    
+
+  delay(1500);
+
+  for (uint16_t pulselen = 150; pulselen < 250; pulselen++) {
+    pwm.setPWM(1, 0, pulselen); //servo 1
+    delay(10);
   }
 
-  delay(2000);
 
-  //servonum ++;
-  //if (servonum > 15) servonum = 0;
+  delay(1500);
+
+  for (uint16_t pulselen = 440; pulselen > 0; pulselen--) {
+    pwm.setPWM(2, 0, pulselen);//servo 2
+    delay(10);
+  }
+
+  delay(1500);
+
+  for (uint16_t pulselen = 250; pulselen > 0; pulselen--) {
+    pwm.setPWM(3, 0, pulselen);//servo 3
+    delay(10);
+  }
+
+  delay(1500);
+
+  for (uint16_t pulselen = 190; pulselen < 300; pulselen++) {
+    pwm.setPWM(5, 0, pulselen); //servo 5
+    delay(10);
+  }
+
+  delay(1500);
+
+//remonter le bras
+  for (uint16_t pulselen = 300; pulselen > 190; pulselen--) {
+    pwm.setPWM(5, 0, pulselen);//servo 5
+    delay(10);
+  }
+
+
+  delay(1500);
+
+  for (uint16_t pulselen = 0; pulselen < 250; pulselen++) {
+    pwm.setPWM(3, 0, pulselen); //servo 3
+    delay(10);
+  }
+
+  delay(1500);
+
+  for (uint16_t pulselen = 0; pulselen < 440; pulselen++) {
+    pwm.setPWM(2, 0, pulselen); //servo 2
+    delay(10);
+  }
+
+  delay(1500);
+
+  for (uint16_t pulselen = 250; pulselen > 150; pulselen--) {
+    pwm.setPWM(1, 0, pulselen);//servo 1
+    delay(10);
+  }
+
+  delay(1500);
+
+  for (uint16_t pulselen = 300; pulselen > 0; pulselen--) {
+    pwm.setPWM(0, 0, pulselen);//servo 0
+    delay(10);
+  }
+
+  delay(1500);  
+
 }
