011727d465ff4d50751e10d8043a09a4b9850e9d
mehdiberriri <mehdi.berriri@etu.unice.fr>
2019-04-27 17:08:57 +0200

    Update _main.ino

--- _main.ino
+++ _main.ino
@@ -1,8 +1,8 @@
 
-Servo servo0 = Servo(0,90,580,0.0,0.0,8.0,2.45,0);
-Servo servo1 = Servo(1,90,450,0.0,0.0,9.5,100.0/45.0,155);
-Servo servo2 = Servo(2,100,580,-10.0,0.0,14,5.0/2.0,90);
-Servo servo3 = Servo(3,90,390,-3.0,0.0,21,200.0/75,90);
+Servo servo0 = Servo(0,90,580,2.45,0);
+Servo servo1 = Servo(1,90,450,100.0/45.0,155);
+Servo servo2 = Servo(2,100,580,5.0/2.0,90);
+Servo servo3 = Servo(3,90,390,200.0/75,90);
 void setup() {
   Serial.begin(9600);
   Serial.println("16 channel Servo test!");
@@ -10,28 +10,64 @@ void setup() {
   pwm.begin();
   
   pwm.setPWMFreq(60);
-
+  
   yield();
 }
 
 void loop() {
 //baisser le bras
-  Serial.println(servonum);
-  for (uint16_t pulselen = 90; pulselen < 570; pulselen++) {
-    pwm.setPWM(4, 0, pulselen); //servo 0
-    delay(10);
+  //Serial.println(servonum);
+  //for (uint16_t pulselen = 90; pulselen < 570; pulselen++) {
+    //pwm.setPWM(4, 0, pulselen); //servo 0
+    //delay(10);
+  //}
+  int x,y,z;
+
+  Serial.println("Début lecture ");
+  for (int i = 0; i < 3; i++) {
+    while(Serial.available() == 0){
+      // do nothing
+    }
+    if(i == 0){
+      x = Serial.parseInt(); // Reading Serial Data and saving in data variable
+      Serial.print("x = ");
+      Serial.println(x); // Printing the Serial data
+    }
+    if(i == 1){
+      y = Serial.parseInt(); // Reading Serial Data and saving in data variable
+      Serial.print("y = ");
+      Serial.println(y); // Printing the Serial data
+    }
+    if(i == 2){
+      z = Serial.parseInt(); // Reading Serial Data and saving in data variable
+      Serial.print("z = ");
+      Serial.println(z); // Printing the Serial data
+    }
   }
-  servo0.moveValeur(300);
-delay(1500);
-  servo0.moveValeur(servo0.calculeMoveDegre(90));
-delay(1500);
-  servo1.moveValeur(servo1.calculeMoveDegre(38));
+  Serial.println("Fin leture");
+  
+  int* tab = new int[5];
+
+  tab = calculeDesAngles(x,y,z);
+  Serial.println("A partir du 1");
+  Serial.print("angle0 ");
+  Serial.println(tab[0]);
+  Serial.print("angle1 ");
+  Serial.println(tab[1]);
+  Serial.print("angle2 ");
+  Serial.println(tab[2]);
+  Serial.print("angle3 ");
+  Serial.println(tab[3]);
+
+  servo0.moveValeur(servo0.calculeMoveDegre(tab[0]));
   delay(1500);
-  servo2.moveValeur(servo2.calculeMoveDegre(136));
-delay(1500);
+  servo1.moveValeur(servo1.calculeMoveDegre(tab[1]));
+  delay(1500);
+  servo2.moveValeur(servo2.calculeMoveDegre(tab[2]));
+  delay(1500);
+  servo3.moveValeur(servo3.calculeMoveDegre(tab[3]));
+  delay(1500);
 
-servo3.moveValeur(servo3.calculeMoveDegre(95));
-delay(1500);
-
+  Serial.println("");
 
 }
