48cb1e4fae47fbf45c1a113a7390abcd0832ca27
mehdiberriri <mehdi.berriri@etu.unice.fr>
2019-06-12 10:28:06 +0200

    Update pronation.ino

--- pronation.ino
+++ pronation.ino
@@ -1,8 +1,9 @@
  #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-uint8_t servonum = 5;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+
 class Servo {
 
   private : 
@@ -59,6 +60,70 @@ class Servo {
 };
 
 
+class Plateau {
+  private : 
+    int echiquier[8][8]; // 1 = pion, 2 = tour, 3 = cavalier, 4 = fou, 5 = reine et roi
+    
+    int n;
+
+    public :
+      Plateau(){
+        this->n = 0;
+        for(int i = 0; i < 8; i++){
+          echiquier[1][i] = 1;
+          echiquier[6][i] = 1;
+        }
+        echiquier[0][0] = 2;
+        echiquier[0][7] = 2;
+        echiquier[7][0] = 2;
+        echiquier[7][7] = 2;
+        
+        echiquier[0][1] = 3;
+        echiquier[0][6] = 3;
+        echiquier[7][1] = 3;
+        echiquier[7][6] = 3;
+
+        echiquier[0][2] = 4;
+        echiquier[0][5] = 4;
+        echiquier[7][2] = 4;
+        echiquier[7][5] = 4;
+
+        echiquier[0][3] = 5;
+        echiquier[0][4] = 5;
+        echiquier[7][3] = 5;
+        echiquier[7][4] = 5;
+        
+      }
+
+      int getEchiquier(int x, int y){
+        return echiquier[x][y];
+      }
+
+      void setEchiquier(int x, int y, int val){
+        echiquier[x][y] = val;
+      }
+
+      int getN(){
+        return n;
+      }
+
+      void setN(int x){
+        n = x;
+      }
+
+      
+      
+      
+};
+
+
+Servo servo0 = Servo(0,90,580,90,2.45,0,10);
+Servo servo1 = Servo(1,90,475,250,2.5,154,10);
+Servo servo2 = Servo(2,100,580,530,2.5,5,10);
+Servo servo3 = Servo(3,90,390,250,2.5,10,10); 
+Servo servo4 = Servo(4,90,570,290,100/45,0,10);
+Servo servo5 = Servo(5,200,450,210,100/45,0,10); 
+
 // indique la position que doit prendre le Servo5 (la pince) pour s'ouvrir selon la taille cm passée en paramètre
 int cmTValeurServo5(float cm){
   if(cm >= 0 || cm <= 15.5) return 100*cm/9 + 200;
@@ -79,7 +144,7 @@ float degreToRadian(float degre){
 
 
 
-int* calculeAngleVariateur42(int x, float y, int z){
+int* calculeAngleVariateur42(float x, float y, float z){
     int AS0 = 0;
     int AS1 = 0;
     int AS2 = 0;
@@ -186,6 +251,67 @@ int* calculeAngleVariateur42(int x, float y, int z){
     return tab;
 }
 
+void mouvementBras(float x, float y, float z, float ouverturePince, float fermeturePince, int typeMouvement){
 
+  Serial.print("X ");
+   Serial.println(x);
 
+   Serial.print("Y ");
+   Serial.println(y);
 
+   Serial.print("Z ");
+   Serial.println(z);
+
+   //CALCULE DES ANGLES
+   int* tab = new int[5];
+   tab = calculeAngleVariateur42(x,y,z);
+   Serial.print("angle0 ");
+   Serial.println(tab[0]);
+   Serial.print("angle1 ");
+   Serial.println(tab[1]);
+   Serial.print("angle2 ");
+   Serial.println(tab[2]);
+   Serial.print("angle3 ");
+   Serial.println(tab[3]);
+
+    
+   //DEBUT DU MOUVEMENT : GESTION DE LA PINCE
+   servo4.moveValeur(servo4.calculeMoveDegre(90));
+   delay(500);
+
+   if(typeMouvement == 0){
+     servo5.moveValeur(cmTValeurServo5(ouverturePince));
+     delay(500);
+   }
+
+   //DESCENTE
+   servo0.moveValeur(servo0.calculeMoveDegre(tab[0]));
+   delay(500);
+   servo3.moveValeur(servo3.calculeMoveDegre(tab[3]));
+   delay(500);
+   servo2.moveValeur(servo2.calculeMoveDegre(tab[2]));
+   delay(500);
+   servo1.moveValeur(servo1.calculeMoveDegre(tab[1]));
+   delay(500);
+  
+
+   //PINCE EN BAS DU MOUVEMENT
+   if(typeMouvement == 0){
+     servo5.moveValeur(cmTValeurServo5(fermeturePince));
+     delay(500);
+   }
+   else{
+     servo5.moveValeur(cmTValeurServo5(ouverturePince));
+     delay(500);
+   }
+
+   //REMONTE
+   servo1.moveValeur(servo1.calculeMoveDegre( 100 ));
+   delay(500);
+   servo2.moveValeur(servo2.calculeMoveDegre( 140 ));
+   delay(500);
+   servo3.moveValeur(servo3.calculeMoveDegre( 150 ));
+   delay(500);
+  
+   Serial.println("");
+}
