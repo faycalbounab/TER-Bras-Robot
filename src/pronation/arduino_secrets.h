ee34193a5a03997bb62c50725346b87c5b31d3f2
mehdiberriri <mehdi.berriri@etu.unice.fr>
2019-06-15 17:18:47 +0200

    Update arduino_secrets.h

--- src/pronation/arduino_secrets.h
+++ src/pronation/arduino_secrets.h
@@ -1,2 +1,2 @@
-#define SECRET_SSID "Becker"
-#define SECRET_PASS "bonjour06200"
+#define SECRET_SSID "nom_reseau" //remplacer par le nom de votre réseau wifi
+#define SECRET_PASS "mot_de_passe" //remplacer par le mot de passe de votre réseau wifi
