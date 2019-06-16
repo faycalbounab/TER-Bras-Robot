3177f237558475c7aca922bd35ccdc2acdd2f3b6
Fayçal Bounab <faycal.bounab@etu.unice.fr>
2019-06-16 15:57:36 +0200

    Update arduino_secrets.h

--- src/pronation/arduino_secrets.h
+++ src/pronation/arduino_secrets.h
@@ -1,2 +1,14 @@
+/*
+ ******************************************************************************
+ * 
+ * Copyright (c) 2019 Groupe : Berriri Mehdi, Bounab Fayçal, Djema Sofiane, Doglio Arthur
+ * All rights reserved.
+ * 
+ * Ce projet s'inscrit dans le cadre d'un Travail d'Etude et de Recherche de Master 1 Informatique.
+ *
+ ******************************************************************************
+ */
+
+
 #define SECRET_SSID "nom_reseau" //remplacer par le nom de votre réseau wifi
 #define SECRET_PASS "mot_de_passe" //remplacer par le mot de passe de votre réseau wifi
